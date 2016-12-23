28d428568eb5750e6163e0d79f2c2f6fda209061
Quint436 <quint436@uw.edu>
2016-12-22 19:19:54 -0800

    adding more stacks

--- stack1.js
+++ stack1.js
@@ -52,3 +52,35 @@ function stack(){
 
 var quint = new stack();
 console.log(quint.push(4).push(44).push(11).push(15).contains(3));
+
+
+//////////////////// More Stacks ////////////////////
+
+function Stack() {
+    var items = [];
+
+    this.push = function(element){
+        items.push(element);
+    };
+    this.pop = function(){
+        return items.pop();
+    };
+    this.isEmpty = function(){
+        return items.length == 0;
+    };
+    this.peek = function(){
+        return items[items.length - 1];
+    };
+    this.size = function(){
+        return items.length;
+    };
+    this.clear = function(){
+        items = [];
+    };
+    this.print = function(){
+        console.log(items.toString());
+    };
+}
+
+var stack1 = new Stack();
+console.log(stack1.isEmpty());
