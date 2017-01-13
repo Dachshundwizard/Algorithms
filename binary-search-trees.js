3c259eba1aa2f5ac5672a2fa05acd28ddd2bb5bf
Quint436 <quint436@uw.edu>
2017-01-12 22:44:17 -0800

    working with Binary Search Trees

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -1,21 +1,48 @@
-///////////Binary Search Trees///////////
+///////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////Binary Search Trees////////////////////////
+//////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////
+       ///////////////////////////////////////////////
+            /////////////////////////////////////
+               /////////////////////////////
+                 ////////////////////////
+                         ///////
+                        ///////
+                         ///////
+                         ///////
+                         ////////////
+                         ///////
+                         ///////
+                         ///////
+                         ///////
+                         ///////
 
-function BST(value){ //  Will have a left and right child node
-    this.value = value; // This value will start
+// Constructor function
+
+function BST(value){            // Will have a left and right child node
+    this.value = value;         // This value will start
     this.left = null;
     this.right = null;
 }
 
-///////////Insert Method///////////
-/////////// This will take in a value, make a new binary search tree (or node), and place that node into our original binary search tree.
+                                                        ///////////Insert Method///////////
+                                                        // This will take in a value, make a new binary search tree (or node),
+                                                        // and place that node into our original binary search tree.
+                                                        ///////////Insert Method///////////
+
+
 BST.prototype.insert = function(value){
     if(value <= this.value) {
-        if(!this.left)this.left = new BST(value);
-        else this.left.insert(value);
+        if(!this.left)this.left = new BST(value);       // If no left child, set left child = to new BST(value)
+        else this.left.insert(value);                   // If there is a left child, then run the new BST on that left child
     }
     else if(value > this.value){
-        if(!this.right)this.right = new BST(value);
-        else this.right.insert(value);
+        if(!this.right)this.right = new BST(value);     // if there is no right child, this.right is a new BST(value)
+        else this.right.insert(value);                  // Otherwise, if there is a right child, pass in the value to the this.right
     }
 };
 
@@ -32,6 +59,29 @@ bst.insert(85);
 bst.insert(105);
 bst.insert(10);
 
-console.log(bst.right.left.left);
-console.log(bst.left.right.left);
-console.log(bst.right.right);
+// console.log(bst.right.left.left);
+// console.log(bst.left.right.left);
+// console.log(bst.right.right);
+
+                                                        ///////////Contains Method///////////
+                                                        // Will tell us is our BST has a given value. If it does contain the value it will return
+                                                        // True, and if it doesnt, it will return false
+                                                        ///////////Contains Method///////////
+
+
+
+BST.prototype.contains = function(value){
+    if(value === this.value) return true;
+    else if(value < this.value){
+        if(!this.left) return false;                    // Is there a left child node? If there isn't, return false
+        else return this.left.contains(value);          // We want to run our contains method on this child node
+    }
+    else if(value > this.value){
+        if(!this.right) return false;
+        else return this.right.contains(value);
+    }
+};
+
+console.log(bst.contains(50));
+console.log(bst.contains(59));
+console.log(bst.contains(15));
