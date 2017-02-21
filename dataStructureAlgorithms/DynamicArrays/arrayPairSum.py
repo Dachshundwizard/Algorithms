2d3a86813e78eec194f65caa00e76c16da8d0e9e
Quint436 <quint436@uw.edu>
2017-02-20 21:53:09 -0800

    0

--- dataStructureAlgorithms/DynamicArrays/arrayPairSum.py
+++ dataStructureAlgorithms/DynamicArrays/arrayPairSum.py
@@ -9,10 +9,10 @@ def pair_sum(arr,k):
 
     if len(arr)<2:
         return
-                                                                                                                # Sets for tracking
+                                                                                                                        # Sets for tracking
     seen = set()
     output = set()
-                                                                                                                # For every number in array
+                                                                                                                        # For every number in array
     for num in arr:
                                                                                                                         # Set target difference
         target = k-num
@@ -20,10 +20,10 @@ def pair_sum(arr,k):
         if target not in seen:
             seen.add(num)
         else:
-                                                                                                                    # Add a tuple with the corresponding pair
+                                                                                                                        # Add a tuple with the corresponding pair
             output.add( (min(num,target),  max(num,target)) )
-                                                                                                                    # FOR TESTING
+                                                                                                                        # FOR TESTING
     # return len(output)
-                                                                                                            # Nice one-liner for printing output
+                                                                                                                        # Nice one-liner for printing output
     return '\n'.join(map(str,list(output)))
 print pair_sum([4,3,2,1], 4)
